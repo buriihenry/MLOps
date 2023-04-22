e616133095c7b7a0a4f683cedfe9f66a73d719c1
buriihenry <buriihenry@gmail.com>
2023-04-22 10:56:01 +0300

    orchestration

---
+++ introduction/orchestration/model_training.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import pickle
+
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.linear_model import LinearRegression, Lasso, Ridge
+from sklearn.metrics import mean_squared_error
+
+import xgboost as xgb
+
