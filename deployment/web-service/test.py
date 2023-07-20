0f3ab3ad13c5f308b801c3529a8c6fdea9bf73d4
buriihenry <buriihenry@gmail.com>
2023-07-20 21:58:15 +0300

    web serive deployment

--- deployment/web-service/test.py
+++ deployment/web-service/test.py
@@ -1,5 +1,4 @@
 import requests
-import predict
 
 ride = {
     "PULocationID": 10,
@@ -7,10 +6,6 @@ ride = {
     "trip_distance": 40
 }
 
-features = predict.prepare_features(ride)
-pred = predict.predict(features)
-print(pred)
-
-# url = 'http://localhost:9696/predict'
-# response = requests.post(url, json=ride)
-# print(response.json())
+url = 'http://localhost:9696/predict'
+response = requests.post(url, json=ride)
+print(response.json())
